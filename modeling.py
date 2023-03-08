56bdee7324a30b6360362698a4143604b3752bf2
Yaoting Wang <ryanwang9966@gmail.com>
2023-03-08 11:09:21 +0800

    设定PA_gate
    接下来要引入... Transformer..

--- modeling.py
+++ modeling.py
@@ -12,22 +12,54 @@ class MAG(nn.Module):
             )
         )
 
-        self.W_hv = nn.Linear(VISUAL_DIM + TEXT_DIM, TEXT_DIM)
-        self.W_ha = nn.Linear(ACOUSTIC_DIM + TEXT_DIM, TEXT_DIM)
+        self.W_hv = nn.Linear(TEXT_DIM + TEXT_DIM, TEXT_DIM)
+        self.W_ha = nn.Linear(TEXT_DIM + TEXT_DIM, TEXT_DIM)
 
-        self.W_v = nn.Linear(VISUAL_DIM, TEXT_DIM)
-        self.W_a = nn.Linear(ACOUSTIC_DIM, TEXT_DIM)
+        self.W_v = nn.Linear(TEXT_DIM, TEXT_DIM)
+        self.W_a = nn.Linear(TEXT_DIM, TEXT_DIM)
         self.beta_shift = beta_shift
 
         self.LayerNorm = nn.LayerNorm(hidden_size)
         self.dropout = nn.Dropout(dropout_prob)
 
+        self.dim_t, self.dim_v, self.dim_a = TEXT_DIM, VISUAL_DIM, ACOUSTIC_DIM
+        self.proj_v = nn.Linear(self.dim_v, self.dim_t)
+        self.proj_a = nn.Linear(self.dim_a, self.dim_t)
+
+        self.pa_gate = nn.parameter.Parameter(torch.ones(3), requires_grad=True)
+
     def forward(self, text_embedding, visual, acoustic):
         eps = 1e-6
+
+        # 1. 先将feature卷积至相同的hidden_dim.
+        visual = self.proj_v(visual)
+        acoustic = self.proj_a(acoustic)
+
+        # 2. PA: pm assign.
+        prob = torch.softmax(self.pa_gate, dim=-1)
+        # print(prob)
+        _pm = torch.argmax(prob)
+        _pm = 2
+        if _pm == 0:
+            ...
+        elif _pm == 1:
+            tmp = text_embedding
+            text_embedding = visual
+            visual = tmp
+        elif _pm == 2:
+            tmp = text_embedding
+            text_embedding = acoustic
+            acoustic = tmp
+        # weight matrices [W] & non-linear activation [R()]
+        # input(f'text: {text_embedding.shape}; visual: {visual.shape}; audio : {acoustic.shape}')
+        # OG: visual: [50, 48, 768], [50, 48, 47], [50, 48, 74]  # [len, bsz, dim]
+        # new: [50, 48, 768].
+
         weight_v = F.relu(self.W_hv(torch.cat((visual, text_embedding), dim=-1)))
         weight_a = F.relu(self.W_ha(torch.cat((acoustic, text_embedding), dim=-1)))
 
-        h_m = weight_v * self.W_v(visual) + weight_a * self.W_a(acoustic)
+        h_m = weight_v * self.W_v(visual) + weight_a * self.W_a(acoustic)  # [50, 48, 768]
+        # input(f'h_m: {h_m.shape}')
 
         em_norm = text_embedding.norm(2, dim=-1)
         hm_norm = h_m.norm(2, dim=-1)
